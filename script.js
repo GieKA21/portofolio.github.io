76531bac36860a1fb4e94178f53862ad6c69e01f
GieKA21 <154489055+GieKA21@users.noreply.github.com>
2023-12-21 14:32:34 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links")
+    const icon = document.querySelector(".hamburger-icon")
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
+
+function sendMail(){
+  let parms = {
+    name : document.getElementById("name").value,
+    number : document.getElementById("number").value,
+    email : document.getElementById("email").value,
+    pesan : document.getElementById("pesan").value
+  }
+  
+  emailjs.send("service_vnx2djr","template_to996vs",parms).then(alert("Email sent!!!"))
+}
